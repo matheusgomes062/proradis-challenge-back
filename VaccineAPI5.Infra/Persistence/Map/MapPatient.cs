4b26a0acc1bc3c3b1ba6a510d7b5aee47d404d2d
matheusgomes062 <matheusgomes062@gmail.com>
2021-05-24 22:30:51 -0300

    feat: added api context

---
+++ VaccineAPI5.Infra/Persistence/Map/MapPatient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VaccineAPI5.Infra.Persistence.Map
+{
+    public class MapPatient
+    {
+        public MapPatient()
+        {
+        }
+    }
+}
--- VaccineAPI5.Infra/Persistence/VaccineAPI5Context.cs
+++ VaccineAPI5.Infra/Persistence/VaccineAPI5Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using VaccineAPI5.Entities;
 
 namespace VaccineAPI5.Infra.Persistence
@@ -16,5 +17,45 @@ namespace VaccineAPI5.Infra.Persistence
         public IDbSet<Patient> Patients { get; set; }
 
         public IDbSet<Vaccine> Vaccines { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Seta o Schema default
+            //modelBuilder.HasDefaultSchema("Apoio");
+
+            //Remove a pluralização dos nomes das tabelas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //Remove exclusão em cascata (permite maior controle)
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //Setar para usar varchar ou invés de nvarchar
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            //Caso eu esqueça de informar o tamanho do campo ele irá colocar varchar de 100
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+            //Mapeia as entidades
+            //modelBuilder.Configurations.Add(new PatientMap());
+            //modelBuilder.Configurations.Add(new VaccineMap());
+
+            #region Adiciona entidades mapeadas - Automaticamente via Assembly
+            modelBuilder.Configurations.AddFromAssembly(typeof(VaccineAPI5Context).Assembly);
+            #endregion
+
+            #region Adiciona entidades mapeadas - Automaticamente via NameSpace
+            //Assembly.GetExecutingAssembly().GetTypes()
+            //    .Where(type => type.Namespace == "VaccineAPI5Context.Domain.Entities.DomainViagem")
+            //    .ToList()
+            //    .ForEach(type =>
+            //    {
+            //        dynamic instance = Activator.CreateInstance(type);
+            //        modelBuilder.Configurations.Add(instance);
+            //    });
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
